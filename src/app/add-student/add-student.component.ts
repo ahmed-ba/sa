801af22064d171cc49e14f9e537eb2b0835af485
Ahmed bahrini <abahrini544@gmail.com>
2020-04-16 23:28:56 +0100

    fixing issues

--- src/app/add-student/add-student.component.ts
+++ src/app/add-student/add-student.component.ts
@@ -1,30 +1,40 @@
-import { Component , OnInit } from '@angular/core' ;
-import { Student } from '../shared/models/student' ;
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Student } from '../shared/models/student';
 import { StudentService } from '../shared/services/student.service';
-@ Component ({
-selector: 'app-add-student' ,
-templateUrl: './add-student.component.html' ,
-styleUrls: [ './add-student.component.css' ]
+import { FormGroup, NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+    selector: 'app-add-student',
+    templateUrl: './add-student.component.html',
+    styleUrls: ['./add-student.component.css']
 })
 export class AddStudentComponent implements OnInit {
-public student = new Student ();
-constructor ( private studentService : StudentService ) { }
-ngOnInit (): void {
-}
-save() {
-    if (!this.student.id) {
-    this.studentService.AddStudent({ ...this.student }).then((res) => {
-    this.formAdd.resetForm();
-    })
-    } else {
-    this.studentService.updateStudent(this.student);
+    public student = new Student();
+    @ViewChild('addForm') addForm: NgForm;
+    constructor(private studentService: StudentService, private route: ActivatedRoute) {
+
     }
+    ngOnInit(): void {
+        this.route.queryParams.subscribe(params => {
+            console.log("My Id", params);
+            this.getStudent(params.id);
+        })
     }
-getStudent(id: string) {
-    this.studentService.getStudent(id).subscribe(res => {
-    this.student = res.data() as Student;
-    this.student.id = res.id;    
-    });
+    save() {
+        if (!this.student.id) {
+            this.studentService.AddStudent({ ... this.student });
+        } else {
+
+            this.studentService.updateStudent({ ... this.student });
+        }
+
+        this.addForm.reset();
+    }
+    getStudent(id: string) {
+        this.studentService.getStudent(id).subscribe(res => {
+            this.student = res.data() as Student;
+            this.student.id = res.id;
+        });
     }
 
 }
